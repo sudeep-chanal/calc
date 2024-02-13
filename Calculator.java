e8695eeb876d4cef9c36f154e36a2824d568e0f4
01fe22bca181 <156196935+sudeep-chanal@users.noreply.github.com>
2024-02-12 21:41:14 -0800

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,57 +1,43 @@
+// The `/** ... */` block is a Java documentation comment. It is used to provide a description or
+// documentation for the class or method that follows it. In this case, it is providing a description
+// of the `calculator` class and its purpose.
 /**
- * The Calculator class is used for performing mathematical calculations.
+ * The calculator class performs basic arithmetic operations such as addition, subtraction,
+ * multiplication, and division on two operands.
  */
-/**
- 
- */
+class calc{ 
 
-class Calculator
-{
-    float addition( float operand_1,float operand_2){
+    float addition(float operand_1,float operand_2){
         return(operand_1+operand_2);
     }
-    float subtraction( float operand_1,float operand_2){
+    float substraction(float operand_1,float operand_2){
         return(operand_1-operand_2);
     }
-    float multiplication( float operand_1,float operand_2){
+    float multiplication(float operand_1,float operand_2){
         return(operand_1*operand_2);
     }
-    float division( float operand_1,float operand_2){
+    float division(float operand_1,float operand_2){
         return(operand_1/operand_2);
     }
+
     float square(float operand_1){
-        return(operand_1*operand_1);
+        return (operand_1*operand_1)
     }
-    public static void main(String[] args) {
-        
-  
+    public static void main(String[]args){
+        calc calc=new calc();
+        float operand_1=10;
+        float operand_2=5;
+        float add_result=calc.addition(operand_1,operand_2);
+        System.out.println("result is:"+add_result);
+        float sub_result=calc.substraction(operand_1,operand_2);
+        System.out.println("result is:"+ sub_result);
+        float mult_result=calc.multiplication(operand_1,operand_2);
+        System.out.println("result is:"+ mult_result);
+        float div_result=calc.division(operand_1,operand_2);
+        System.out.println("result is:"+ div_result);
 
-float operand_1=10;
-float operand_2=20;
-
- 
- float add_result=calc.addition(operand_1,operand_2);
- System.out.println("result of subtraction is:"+add_result);
-
-            float sub_result=calc.subtraction(operand_1,operand_2);
-    System.out.println("result of subtraction is:"+sub_result);
-
-    float mul_result=calc.multiplication(operand_1,operand_2);
-    System.out.println("result of multiplication is:"+mul_result);
-
-    float div_result=calc.division(operand_1,operand_2);
-    System.out.println("result of division:"+div_result);
-
-    float sq_result=calc.square(operand_1);
-    System.out.println("result of square is:"+sq_result);
-
-    float cube_result=calc.cube(operand_1);
-    System.out.println("result of cube is:"+cube_result);
-
-    float modulus_result=calc.mod(operand_1,operand_2);
-    System.out.println("result of mod is:"+modulus_result);
-
-   
+        float squ_result=calc.division(operand_1,operand_2);
+        System.out.println("result is:"+ squ_result);
+gbfrgegtehqet5gx
     }
-       
-    }
+}
